46cdd9b6a71344626eced42b9214d2385eab3af5
Christian Hartlage <c@dende.de>
2020-02-24 18:22:11 +0100

    fix int to enum converstion having the wrong range

--- projects/libqrcodegen/encode_binary_cpp_fuzzer.cpp
+++ projects/libqrcodegen/encode_binary_cpp_fuzzer.cpp
@@ -8,7 +8,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
   auto ecc_byte = (int) Data[0];
   Data++;Size--;
-  qrcodegen::QrCode::Ecc ecl = static_cast<qrcodegen::QrCode::Ecc> (ecc_byte % 5);
+  qrcodegen::QrCode::Ecc ecl = static_cast<qrcodegen::QrCode::Ecc> (ecc_byte % 4);
 
   std::vector<uint8_t> dataVector(Data, Data + Size);
   auto qr = qrcodegen::QrCode::encodeBinary(dataVector, ecl);
