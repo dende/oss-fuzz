dce661023ffb3fcacb0517f44ef6fd3e980cb901
Christian Hartlage <c@dende.de>
2020-02-24 18:24:19 +0100

    fix uint8_t buffers not being big enough

--- projects/libqrcodegen/encode_binary_c_fuzzer.cpp
+++ projects/libqrcodegen/encode_binary_c_fuzzer.cpp
@@ -1,5 +1,6 @@
-#include <stdbool.h>
-#include <stdint.h>
+#include <cstdbool>
+#include <cstdint>
+#include <cstring>
 #include "qrcodegen.h"
 
 // Text data
@@ -7,7 +8,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   if (Size < 2)
     return 0;
 
-  uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
+  uint8_t dataAndTemp[qrcodegen_BUFFER_LEN_FOR_VERSION(qrcodegen_VERSION_MAX)];
+  memcpy(dataAndTemp, Data, Size);
+
+  uint8_t qr0[qrcodegen_BUFFER_LEN_FOR_VERSION(qrcodegen_VERSION_MAX)];
   bool ok = qrcodegen_encodeBinary((uint8_t *) Data, Size, qr0, qrcodegen_Ecc_MEDIUM, qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_4, true);
   if (!ok)
       return 1;
